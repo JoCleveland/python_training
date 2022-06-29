09ae564f5348ef84bf49415cec45c53879dbbcd5
JoCleveland <cleveland.louise2004@gmail.com>
2022-06-29 10:22:21 -0700

    updates

--- Tweepy_bots/configuration.py
+++ Tweepy_bots/configuration.py
@@ -1,10 +1,18 @@
 from math import e
-from tkinter import E
+
+
+
 import tweepy
+
+
 import logging
+
+
 import os
 
+
 logger = logging.getLogger()
+
 
 def create_api():
     api_key = os.getenv("API_KEY")
@@ -17,13 +25,18 @@ def create_api():
     auth.set_access_token(access_token,access_token_secret)
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
+
     try:
         api.verify_credentials()
+
 
     except Exception as e:
         logger.error("Error creating API", exc_info=True)
         raise e
     logger.info("API created")
+    
+    
     return api
+
 
     
---
+++ Tweepy_bots/duck_bot.py
@@ -0,0 +1,104 @@
+import tweepy
+
+
+from math import e
+
+
+import logging
+
+
+from configuration import create_api
+
+
+import time
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+class FavRetweetListner(tweepy.StreamListner):
+
+
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
+
+
+    def on_status(self, tweet):
+        logger.info(f'Processing tweet with id {tweet.id}.')
+
+
+        if tweet.in_reply_to_status_id is not None or tweet.user.id \
+            == self.me.id:
+            return
+
+
+        if not tweet.favorited:
+            try:
+                tweet.favorite()
+
+
+            except Exception as e:
+                logger.error("An error occured while favoriting.", exc_info=True)
+
+        
+        if not tweet.retweeted:
+            try:
+                tweet.retweet()
+
+
+            except Exception as e:
+                logger.error("An error occured while retweeting.", exc_info=True)
+
+
+    def on_error(self, status):
+        logger.error(status)
+
+
+def check_mentions(api, keywords, since_id):
+    logger.info("Retrieving mentions")
+    new_since_id = since_id
+
+
+    for tweet in tweepy.Cursor(api.mentions_timeline,
+    since_id=since_id).items():
+        new_since_id = max(tweet.id, new_since_id)
+
+
+        if tweet.in_reply_to_status_id is not None:
+            continue
+
+
+        if any(keyword in tweet.text.lower() for keyword in keywords):
+            logger.info(f"Answering {tweet.user.name}'s tweet.")
+
+            api.update_status(
+                status="Please contact me via DM.",
+                in_reply_to_status_id=tweet.id,
+            )
+
+
+    return new_since_id
+
+
+def main(keywords):
+    api = create_api()
+    tweets_listner = FavRetweetListner(api)
+    stream = tweepy.Stream(api.auth, tweets_listner)
+    stream.filter(track=keywords, languages=["en"])
+
+    since_id = 1
+    while True:
+        since_id = check_mentions(api,["help", "care", "duck"], since_id)
+        logger.info("Waiting...")
+        time.sleep(300)
+
+
+
+
+
+if __name__ == "__main__":
+
+
+    main("duck", "ducks")
