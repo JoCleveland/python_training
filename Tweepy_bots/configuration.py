b27e16f28f49a718fdf9a1af5afdfaec3fba42b5
JoCleveland <cleveland.louise2004@gmail.com>
2022-06-29 09:27:38 -0700

    copied all bots

---
+++ Tweepy_bots/configuration.py
@@ -0,0 +1,29 @@
+from math import e
+from tkinter import E
+import tweepy
+import logging
+import os
+
+logger = logging.getLogger()
+
+def create_api():
+    api_key = os.getenv("API_KEY")
+    api_key_secret = os.getenv("API_KEY_SECRET")
+    access_token = os.getenv("ACCESS_TOKEN")
+    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
+    
+    auth = tweepy.OAuthHandler(api_key, api_key_secret)
+    auth.set_access_token(access_token,access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+    try:
+        api.verify_credentials()
+
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+    return api
+
+    
---
+++ Tweepy_bots/favretweet.py
@@ -0,0 +1,66 @@
+import tweepy
+
+
+import logging
+
+
+from configuration import create_api
+
+
+import json
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+class FavRetweetListner(tweepy.StreamListner):
+    
+    
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
+
+    
+    def on_status (self, tweet):
+        logger.info(f'Processing tweet id {tweet.id}')
+        
+        
+        if tweet.in_reply_to_status_id is not None or \
+            tweet.user.id == self.me.id:
+            return
+        
+        
+        if not tweet.favorited:
+            try:
+                tweet.favorite()
+
+
+            except Exception as e:
+                logger.error("Error on favorite", exc_info=True)
+       
+       
+        if not tweet.retweeted:
+            try:
+                tweet.retweet()
+
+
+            except Exception as e:
+                logger.error("Error on retweet", exc_info=True)
+
+        
+    def on_error(self, status):
+        logger.error(status)
+
+    
+def main(keywords):
+    api = create_api()
+    tweets_listner = FavRetweetListner(api)
+    stream = tweepy.Stream(api.auth, tweets_listner)
+    stream.filter(track=keywords, languages=["en"])
+
+
+if __name__ == "__main__":
+    
+    
+    main(["cactus","succulent"])
