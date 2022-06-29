81036fb1974d84ab027d74724f44bb83b61c9822
JoCleveland <cleveland.louise2004@gmail.com>
2022-06-29 12:06:47 -0700

    small changes

--- Bots/configuration.py
+++ Bots/configuration.py
@@ -14,13 +14,13 @@ logger = logging.getLogger()
 
 
 def create_api():
-    api_key = os.getenv("API_KEY")
-    api_key_secret = os.getenv("API_KEY_SECRET")
+    consumer_key = os.getenv("CONSUMER_KEY")
+    consumer_secret = os.getenv("CONSUMER_SECRET")
     access_token = os.getenv("ACCESS_TOKEN")
     access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-    
-    auth = tweepy.OAuthHandler(api_key, api_key_secret)
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token,access_token_secret)
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
@@ -38,4 +38,5 @@ def create_api():
     return api
 
 
-    
+if __name__ == "__main__":
+    create_api()
